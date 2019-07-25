edee61e407a8d21bde68abe812a4754987e0962b
osor1o <osor1o@hotmail.com>
2019-07-25 08:29:54 -0300

    Add Grid, IconButton, If, Form, List

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -7,13 +7,13 @@ export default props => (
             <Link to="/todo" className="navbar-brand">
                 <i className="fa fa-calendar-check"></i> TodoApp
             </Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
             <div className="navbar-collapse collapse">
                 <ul className="navbar-nav mr-auto">
-                    <li class="nav-item"><Link to="/todo" className="nav-link">Todo</Link></li>
-                    <li class="nav-item"><Link to="/about" className="nav-link">About</Link></li>
+                    <li className="nav-item"><Link to="/todo" className="nav-link">Todo</Link></li>
+                    <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
                 </ul>
             </div>
         </div>
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
+export default props => (
+    <div role="form" className="todoForm form-row">
+        <Grid cols="12 9 10">
+            <input 
+                id="description" className="form-control" 
+                placeholder="Adicione uma tarefa"
+                onChange={props.handleChange}
+                value={props.description}
+            />
+        </Grid>
+        <Grid cols="12 3 2">
+            <IconButton bg="primary" icon="plus" onClick={props.handleAdd} />
+        </Grid>
+    </div>
+)
