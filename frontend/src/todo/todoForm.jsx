447844837bd682dee47845345b4db4956a5115cd
Osorio Neto <osor1o@hotmail.com>
2019-07-25 13:45:01 -0300

    finished without redux

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role="form" className="todoForm form-row">
-        <Grid cols="12 9 10">
-            <input 
-                id="description" className="form-control" 
-                placeholder="Adicione uma tarefa"
-                onChange={props.handleChange}
-                value={props.description}
+export default props => {
+
+    const keyHandler = e => {
+        if (e.key === 'Enter')
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        if (e.key === 'Escape')
+            props.handleClear()
+    }
+
+    return (
+        <div role="form" className="todoForm form-row">
+            <Grid cols="12 9 10">
+                <input 
+                    id="description" className="form-control" 
+                    placeholder="Adicione uma tarefa"
+                    onChange={props.handleChange}
+                    value={props.description}
+                    onKeyUp={keyHandler}
             />
-        </Grid>
-        <Grid cols="12 3 2">
-            <IconButton bg="primary" icon="plus" onClick={props.handleAdd} />
-        </Grid>
-    </div>
-)
+            </Grid>
+            <Grid cols="12 3 2" className="text-center">
+                <IconButton bg="primary" icon="plus" onClick={props.handleAdd} />
+                <IconButton bg="info" icon="search" onClick={props.handleSearch} />
+                <IconButton bg="default" icon="times" onClick={props.handleClear} />
+            </Grid>
+        </div>
+    )
+}
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
 
-export default props => (
-    <div>
-        <h3>List</h3>
-    </div>
-)
+import IconButton from '../template/iconButton'
+
+export default props => {
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone': ''}>
+                    {todo.description}
+                </td>
+                <td className="text-center">
+                    <IconButton
+                        bg="success" icon="check"
+                        onClick={() => props.handleMarkAsDone(todo)}
+                        hide={todo.done}
+                    ></IconButton>
+                     <IconButton
+                        bg="warning" icon="undo"
+                        onClick={() => props.handleMarkAsPending(todo)}
+                        hide={!todo.done}
+                    ></IconButton>
+                    <IconButton
+                        bg="danger" icon="trash"
+                        onClick={() => props.handleRemove(todo)}
+                        hide={!todo.done}
+                    ></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className="mt-4 table table-bordered">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th className="tableActions">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
